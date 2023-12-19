2888fd3767c29c7c7032f0e44aeb07ea9184f5e4
Elia Franc <efranc98@gmail.com>
2023-12-19 17:37:02 +0100

    First tests with prediction function.

--- rts-tracker/model.py
+++ rts-tracker/model.py
@@ -1,8 +1,9 @@
 from typing import List, Dict, Optional
 from label_studio_ml.model import LabelStudioMLBase
 
+from pytracking.pytracking.evaluation import Tracker
 
-class NewModel(LabelStudioMLBase):
+class RTSTracker(LabelStudioMLBase):
 
     def predict(self, tasks: List[Dict], context: Optional[Dict] = None, **kwargs) -> List[Dict]:
         """ Write your inference logic here
@@ -10,13 +11,55 @@ class NewModel(LabelStudioMLBase):
             :param context: [Label Studio context in JSON format](https://labelstud.io/guide/ml.html#Passing-data-to-ML-backend)
             :return predictions: [Predictions array in JSON format](https://labelstud.io/guide/export.html#Raw-JSON-format-of-completed-tasks)
         """
-        print(f'''\
-        Run prediction on {tasks}
-        Received context: {context}
-        Project ID: {self.project_id}
-        Label config: {self.label_config}
-        Parsed JSON Label config: {self.parsed_label_config}''')
-        return []
+
+        tracker = Tracker('rts', 'rts50')
+        predictions = []
+        print(tasks)
+        # TODO: For now lets assume one annotation / one object
+        first_annotations = tasks[0]['annotations']
+        video_path = tasks[0]['data']['video']
+
+        for first_annotation in first_annotations:
+            result = first_annotation['result'][0]['value']['sequence'][0]
+            rel_bbox = [result['x'], result['y'], result['width'], result['height']]
+            label_id = first_annotation['result'][0]['id']
+            # tracker.run_video_noninteractive(videofilepath=video_path, optional_box=rel_bbox)
+
+        results = []
+        sequence = []
+        for i in range(50):
+            # creates a random ID for your label everytime so no chance for errors
+            sequence.append({
+                    'frame': i + 2,
+                    'enabled': 'true',
+                    'rotation': 0,
+                    'x': rel_bbox[0],
+                    'y': rel_bbox[1],
+                    'width': rel_bbox[2],
+                    'height': rel_bbox[3],
+                    'time': 0.04,
+                    })
+
+        results.append({
+            'id': label_id,
+            'from_name': "box",
+            'to_name': "video",
+            'type': "videorectangle",
+            'origin': "ml-backend",
+            'image_rotation': 0,
+            'value': {
+                'sequence': sequence,
+                'labels': ["drone"],
+            },
+            'score': 0.5,
+            'readonly': False
+        })
+
+        return [{
+            'result': results,
+            'model_version': 0
+        }]
+
 
     def fit(self, event, data, **kwargs):
         """
@@ -42,3 +85,10 @@ class NewModel(LabelStudioMLBase):
 
         print('fit() completed successfully.')
 
+    def relative_to_absolute_bb(value, total_height, total_width):
+        tl_x = total_width * (value['x'] / 100)
+        tl_y = total_height * (value['y'] / 100)
+        bb_width = total_width / value['width']
+        bb_height = total_height / value['height']
+
+        return [tl_x, tl_y, bb_width, bb_height]
